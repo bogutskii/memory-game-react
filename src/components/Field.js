fdce6606ccd4a739403271fbc2c966ecf4ba9202
bogutskii <sl9site@gmail.com>
2021-01-31 00:30:14 -0800

    -img

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,21 +2,10 @@ import React from 'react';
 import Card from "./Card/Card";
 
 
-const Field = () => {
+const Field = (props) => {
 
     return <>
 
-        <button type="button" className="btn btn-primary">Primary</button>
-
-
-        <div className="mb-3">
-            <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-        </div>
-        <div className="mb-3">
-            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
 
     </>
 }
